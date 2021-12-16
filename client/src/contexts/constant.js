13f2c0fa9414a43e937458a2d5e5c0ae74c372dd
vu chung <vuchung682001@gmail.com>
2021-12-16 14:49:32 +0700

    Done

--- client/src/contexts/constant.js
+++ client/src/contexts/constant.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== 'production'
     ? 'https://chungblogapp.herokuapp.com/api'
-    : 'vovan';
+    : 'https://chungblogapp.herokuapp.com/api';
 export const LOCAL_STORAGE_TOKEN_NAME = 'chung';
 
 export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS';
